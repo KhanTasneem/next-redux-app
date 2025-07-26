77bbe67d017e5f2dc2c954cbd0ccabe9f246a95a
Seher Khan <seherkhan@Sehers-MacBook-Pro.local>
2025-07-26 19:17:36 +0530

    some styling done

--- src/app/components/Counter.tsx
+++ src/app/components/Counter.tsx
@@ -2,12 +2,16 @@
 import { useDispatch, useSelector } from "react-redux";
 import { IRootState } from "../redux/store";
 import { decrement, increment } from "../redux/counterSlice";
-
+import { Metadata } from "next";
+export const metadeta: Metadata={
+  title: "Counter App",
+  }
 const Counter = () => {
   const dispatch = useDispatch();
   const counter = useSelector((state:IRootState) => state.counter.value);
+  
   return (
-    <div>
+    <div className="mx-auto border p-5 m-5 w-100 flex flex-col items-center justify-center space-y-2">
       <div>Counter: {counter}</div>
       <button className="bg-blue-500 text-white px-4 py-2 rounded" onClick={() => dispatch(increment())}>
         Increment
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Counter from "./components/Counter";
 export default function Home() {
   return (
     <div>
-      <h1 className="text-2xl font-bold">Welcome to the Home Page</h1>
+      <h1 className="text-2xl font-bold text-center">Welcome to the Home Page</h1>
       <Counter/>
     </div>
   );
